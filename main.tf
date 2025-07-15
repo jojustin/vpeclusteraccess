fe83db7058856150874a50ddba9f3a9a1cfeddf4
Josephine Eskaline Joyce <jojustin@in.ibm.com>
2025-07-15 13:04:50 +0530

    update

--- main.tf
+++ main.tf
@@ -2,15 +2,15 @@ data "ibm_resource_group" "resource_group" {
   name = "Default"
 }
 
-data "ibm_container_vpc_cluster" "cluster" {
-  name              = "roks-vpc-cluster-jej"
-  resource_group_id = data.ibm_resource_group.resource_group.id
-}
+# data "ibm_container_vpc_cluster" "cluster" {
+#   name              = "roks-vpc-cluster-jej"
+#   resource_group_id = data.ibm_resource_group.resource_group.id
+# }
 
-resource "kubernetes_namespace" "example" {
-  metadata {
-    name = "jej-eg-namespace"
-  }
-  depends_on = [ ibm_container_vpc_cluster.cluster ]
-}
+# resource "kubernetes_namespace" "example" {
+#   metadata {
+#     name = "jej-eg-namespace"
+#   }
+#   depends_on = [ ibm_container_vpc_cluster.cluster ]
+# }
 
--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,14 @@
-# output "vsi_public_ip" {
-#   description = "Network interfaces"
-#   value = ibm_is_instance.vsi.primary_network_interface[0]
+output "rg" {
+  value = data.ibm_resource_group.resource_group.id
+}
+
+# output "cluster" {
+#   value = data.ibm_resource_group.resource_group.id
 # }
 
-# output "vsi_floating_ip" {
-#   description = "Public IP address assigned to the VSI"
-#   value       = ibm_is_floating_ip.fip.address
+# output "namespaceinfo" {
+#  value = kubernetes_namespace.example.id
 # }
-output "namespaceinfo" {
- value = kubernetes_namespace.example.id
-}
 
 # output "namespaceinfo1" {
 #  value = kubernetes_namespace.example.metadata
