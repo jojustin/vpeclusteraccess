410dc3865c7b6790b3467dd8a9f5796e2d464473
Josephine Eskaline Joyce <jojustin@in.ibm.com>
2025-07-15 12:35:45 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = ">= 1.60.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.37.1"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.region
