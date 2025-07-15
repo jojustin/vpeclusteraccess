04e4845a74388dce2a3a0e2463a1ef449f5262cf
Josephine Eskaline Joyce <jojustin@in.ibm.com>
2025-07-15 13:27:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ data "ibm_container_vpc_cluster" "cluster" {
   # resource_group_id = data.ibm_resource_group.resource_group.id
 }
 
-# resource "kubernetes_namespace" "example" {
-#   metadata {
-#     name = "jej-eg-namespace"
-#   }
-#   depends_on = [ ibm_container_vpc_cluster.cluster ]
-# }
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "jej-eg-namespace"
+  }
+  depends_on = [ ibm_container_vpc_cluster.cluster ]
+}
 
