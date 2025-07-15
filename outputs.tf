79a8353730851c84e86acf9935b2211f72136009
Josephine Eskaline Joyce <jojustin@in.ibm.com>
2025-07-15 13:40:25 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -10,9 +10,9 @@ output "clusterconfig" {
   value = data.ibm_container_cluster_config.cluster_config.cluster_name_id
 }
 
-# output "namespaceinfo" {
-#  value = kubernetes_namespace.example.id
-# }
+output "namespaceinfo" {
+ value = kubernetes_namespace.example.id
+}
 
 # output "namespaceinfo1" {
 #  value = kubernetes_namespace.example.metadata
